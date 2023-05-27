a49129a75f29ff5e4f053b2292b60eba9b417144
aarminoe <aarminoe@gmail.com>
2023-05-27 09:42:24 -0500

    added cost of items to trim page

--- src/Rough.js
+++ src/Rough.js
@@ -75,8 +75,19 @@ function Rough() {
                 </div>
             </div>
             <div class='nested'>
-                QUANTITY
                 SOLD                                                     
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
                 <div class='nested'>___</div>
                 <div class='nested'>___</div>
                 <div class='nested'>___</div>
@@ -90,12 +101,81 @@ function Rough() {
             </div>
             <div class='nested'>
                 INSTALLED
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
             </div>
             <div class='nested'>
                 COST
+                <div class='nested'>$3.00</div>
+                <div class='nested'>$3.00</div>
+                <div class='nested'>$35.00</div>
+                <div class='nested'>$70.00</div>
+                <div class='nested'>$125.00</div>
+                <div class='nested'>$20.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$60.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$25.00</div>
+                <div class='nested'>$45.00</div>
+                <div class='nested'>$25.00</div>
+                <div class='nested'>$40.00</div>
+                <div class='nested'>$60.00</div>
+                <div class='nested'>$90.00</div>
+                <div class='nested'>$20.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$45.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$55.00</div>
+                <div class='nested'>$80.00</div>
+                <div class='nested'>$10.00</div>
+                <div class='nested'>$55.00</div>
             </div>
             <div class='nested'>
                 TOTAL
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
+                <div class='nested'>___</div>
             </div>
         </div>
     )

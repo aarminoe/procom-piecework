50fd6eff33950de5e5c2855a2a5474b9e89c0300
aarminoe <aarminoe@gmail.com>
2023-05-29 12:43:45 -0500

    working on activation page

--- src/Activation.js
+++ src/Activation.js
@@ -2,8 +2,258 @@ import React from "react";
 
 function Activation() {
     return(
-        <div>
-            activation page
+        <div class='grid-container'>
+            <div >
+                <div class='nested'>
+                    CODE
+                </div>
+                <div class='nested'>
+                    100502
+                </div>
+                <div class='nested'>
+                   -
+                </div>
+                <div class='nested'>
+                    102501
+                </div>
+                <div class='nested'>
+                    102520
+                </div>
+                <div class='nested'>
+                    102523
+                </div>
+                <div class='nested'>
+                    102526
+                </div>
+                <div class='nested'>
+                    102528
+                </div>
+                <div class='nested'>
+                    102532
+                </div>
+                <div class='nested'>
+                    102534
+                </div>
+                <div class='nested'>
+                    102542
+                </div>
+                <div class='nested'>
+                    102543
+                </div>
+                <div class='nested'>
+                    102590
+                </div>
+                <div class='nested'>
+                    102592
+                </div>
+                <div class='nested'>
+                    102593
+                </div>
+                <div class='nested'>
+                    102595
+                </div>
+                <div class='nested'>
+                    103101
+                </div>
+                <div class='nested'>
+                    104461
+                </div>
+                <div class='nested'>
+                    104462
+                </div>
+                <div class='nested'>
+                    104463
+                </div>
+                <div class='nested'>
+                    104464
+                </div>
+                <div class='nested'>
+                    104465
+                </div>
+                <div class='nested'>
+                    107101
+                </div>
+                <div class='nested'>
+                    108806
+                </div>
+                <div class='nested'>
+                    108806
+                </div>
+                <div class='nested'>
+                    108807
+                </div>
+                <div class='nested'>
+                    108807
+                </div>
+                <div class='nested'>
+                    108201
+                </div>
+                <div class='nested'>
+                    109905
+                </div>
+                <div class='nested'>
+                    109917
+                </div>
+                <div class='nested'>
+                    109918
+                </div>
+                <div class='nested'>
+                    110011
+                </div>
+                <div class='nested'>
+                    110021
+                </div>
+                <div class='nested'>
+                    111115
+                </div>
+                <div class='nested'>
+                    111123
+                </div>
+                <div class='nested'>
+                    111101
+                </div>
+                <div class='nested'>
+                    111102
+                </div>
+                <div class='nested'>
+                    111105
+                </div>
+                <div class='nested'>
+                    111106
+                </div>
+            </div>
+            <div class='nested'>
+                DESCRIPTION
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+            </div>
+            <div class='nested'>
+                ACTIVATION
+                <div class='nested'>$80.00</div>
+                <div class='nested'>$100.00 w/ Doorbell</div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+                <div class='nested'></div>
+            </div>
+            <div class='nested'>
+                STAND ALONE ACTIVATION
+                <div class='nested'>$3.00</div>
+                <div class='nested'>$3.00</div>
+                <div class='nested'>$35.00</div>
+                <div class='nested'>$70.00</div>
+                <div class='nested'>$125.00</div>
+                <div class='nested'>$20.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$60.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$25.00</div>
+                <div class='nested'>$45.00</div>
+                <div class='nested'>$25.00</div>
+                <div class='nested'>$40.00</div>
+                <div class='nested'>$60.00</div>
+                <div class='nested'>$90.00</div>
+                <div class='nested'>$20.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$45.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$55.00</div>
+                <div class='nested'>$80.00</div>
+                <div class='nested'>$10.00</div>
+                <div class='nested'>$55.00</div>
+            </div>
+            <div class='nested'>
+                TOTAL
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'><input class='nested-input'/></div>
+            </div>
         </div>
     )
 }

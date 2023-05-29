ee8fc13c45c97e4b0edfbe8d22b3128c56f526f4
aarminoe <aarminoe@gmail.com>
2023-05-29 07:46:57 -0500

    fixed rough costs, fixed trim names

--- src/Rough.js
+++ src/Rough.js
@@ -126,31 +126,27 @@ function Rough() {
             </div>
             <div class='nested'>
                 COST
-                <div class='nested'>$3.00</div>
-                <div class='nested'>$3.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$25.00</div>
+                <div class='nested'>$30.00</div>
+                <div class='nested'>$45.00</div>
+                <div class='nested'>$60.00</div>
+                <div class='nested'>$15.00</div>
+                <div class='nested'>$20.00</div>
                 <div class='nested'>$35.00</div>
-                <div class='nested'>$70.00</div>
-                <div class='nested'>$125.00</div>
+                <div class='nested'>$25.00</div>
+                <div class='nested'>$75.00</div>
+                <div class='nested'>$100.00</div>
                 <div class='nested'>$20.00</div>
                 <div class='nested'>$15.00</div>
-                <div class='nested'>$60.00</div>
-                <div class='nested'>$30.00</div>
                 <div class='nested'>$25.00</div>
-                <div class='nested'>$45.00</div>
-                <div class='nested'>$25.00</div>
-                <div class='nested'>$40.00</div>
+                <div class='nested'>$50.00</div>
                 <div class='nested'>$60.00</div>
-                <div class='nested'>$90.00</div>
-                <div class='nested'>$20.00</div>
-                <div class='nested'>$15.00</div>
-                <div class='nested'>$30.00</div>
-                <div class='nested'>$45.00</div>
-                <div class='nested'>$15.00</div>
-                <div class='nested'>$30.00</div>
-                <div class='nested'>$55.00</div>
-                <div class='nested'>$80.00</div>
                 <div class='nested'>$10.00</div>
                 <div class='nested'>$55.00</div>
+                <div class='nested'>$50.00</div>
+                <div class='nested'>$30.00</div>
+
             </div>
             <div class='nested'>
                 TOTAL
--- src/Trim.js
+++ src/Trim.js
@@ -2,7 +2,87 @@ import React from "react";
 
 function Trim() {
     return(
-        <div>
+        <div class='grid-container'>
+            <div >
+                <div class='nested'>
+                    STRUCTURED WIRING - ROUGH-IN DESCRIPTION
+                </div>
+                <div class='nested'>
+                    Termination of Cat5/6, RG6 keystone
+                </div>
+                <div class='nested'>
+                    Blank Plate Installation
+                </div>
+                <div class='nested'>
+                    Terminating LV Enclosure up to 12 wires
+                </div>
+                <div class='nested'>
+                    Terminating LV Enclosure up to 25 wires
+                </div>
+                <div class='nested'>
+                    Terminating LV Enclosure up to 50 wires
+                </div>
+                <div class='nested'>
+                    Terminating door contact
+                </div>
+                <div class='nested'>
+                    Terminating other alarm devices
+                </div>
+                <div class='nested'>
+                    Terminating alarm panel
+                </div>
+                <div class='nested'>
+                    Terminating zone expander (8-zone)
+                </div>
+                <div class='nested'>
+                    Speaker Installation With Brackets (pair)
+                </div>
+                <div class='nested'>
+                    Speaker Installation Without Brackets (pair)
+                </div>
+                <div class='nested'>
+                    Outdoor Speakers Installation (pair)
+                </div>
+                <div class='nested'>
+                    Digital Audio System
+                </div>
+                <div class='nested'>
+                    Terminating 5.1-2 Surround (ceiling/wall)
+                </div>
+                <div class='nested'>
+                    Terminating 7.1-2 Surround (ceiling/wall)
+                </div>
+                <div class='nested'>
+                    Terminating Surround Plate
+                </div>
+                <div class='nested'>
+                    Terminating Audio Keypad/VC/Source/TP
+                </div>
+                <div class='nested'>
+                    Camera Installation & Positioning (per camera)
+                </div>
+                <div class='nested'>
+                    NVR/DVR Trim & Installation
+                </div>
+                <div class='nested'>
+                    Network Device Installation
+                </div>
+                <div class='nested'>
+                    Network Switch Trim up to 8Port
+                </div>
+                <div class='nested'>
+                    Network Switch Trim up to 16Port
+                </div>
+                <div class='nested'>
+                    Network Switch Trim up to 24Port
+                </div>
+                <div class='nested'>
+                    Vacuum Trim (outlets)
+                </div>
+                <div class='nested'>
+                    Vacuum Trim Unit
+                </div>
+            </div>
             <div class='nested'>
                 COST
                 <div class='nested'>$3.00</div>

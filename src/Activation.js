30527d8ac5f535830199989c2ea2ed95d243ba7b
aarminoe <aarminoe@gmail.com>
2023-05-31 18:36:43 -0500

    added all descriptions to activation

--- src/Activation.js
+++ src/Activation.js
@@ -130,46 +130,46 @@ function Activation() {
             </div>
             <div class='nested'>
                 DESCRIPTION
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
-                <div class='nested'><input class='nested-input'/></div>
+                <div class='nested'>ClareOne TechConnect Package</div>
+                <div class='nested'>-</div>
+                <div class='nested'>ClareOne Safe and Secure Package (2)DC, (1GB), (1) Water, (1) PIR</div>
+                <div class='nested'>ClareOne Auxiliary TouchPanel</div>
+                <div class='nested'>ClareOne Mini Door/Window Contact</div>
+                <div class='nested'>ClareOne Glass Break Sensor</div>
+                <div class='nested'>ClareOne PIR Motion Detector (Pet Immune to 55 lbs.)</div>
+                <div class='nested'>ClareOne Smoke/Heat Detectors</div>
+                <div class='nested'>ClareOne Sump Pump Water Sensor</div>
+                <div class='nested'>ClareOne Pendant Panic Button</div>
+                <div class='nested'>ClareOne Carbon Monoxide Detector</div>
+                <div class='nested'>Basic Interactive (No Security)</div>
+                <div class='nested'>Interactive (Self Monitoring)</div>
+                <div class='nested'>Interactive Plus IP only</div>
+                <div class='nested'>Interactive Plus LTE + IP Backup</div>
+                <div class='nested'>Video Doorbell</div>
+                <div class='nested'>Sonos Single Zone Music System w/ In-Ceiling Speakers</div>
+                <div class='nested'>Sonos SoundBar Music System (No Subwoofer)</div>
+                <div class='nested'>Sonos Soundbar Music System w/ Subwoofer</div>
+                <div class='nested'>Sonos Surround Sound Music System w/ Klipsch Speakers (No Subwoofer)</div>
+                <div class='nested'>Sonos Surround Music System w/ Klipsch Speakers, Subwoofer and Sonos Amp</div>
+                <div class='nested'>ClareOne Smart Lighting Package 2+2+2</div>
+                <div class='nested'>Pulte Smart Home Option 1 (Single WAP)</div>
+                <div class='nested'>Pulte Smart Home Option 1 (Two WAPs)</div>
+                <div class='nested'>Del Webb Smart Home Option 1 (Single WAP)</div>
+                <div class='nested'>Del Webb Smart Home Option 1 (Two WAPs)</div>
+                <div class='nested'>Home Away - (3 DC, 1 Motion Detector, Garage Opener and Smart Lock By Others)</div>
+                <div class='nested'>House WiFi Package: (2) WAP UniFi, (1) Power Conditioner, (1) 8-Port Switch with 4 PoE Ports</div>
+                <div class='nested'>Wireless Access Point (No Wiring Included)</div>
+                <div class='nested'>OutDoor Wireless Access Point - UniFi</div>
+                <div class='nested'>Central Vacuum Solution #1B - 4 Inlets, Kit With Power Head and Hose</div>
+                <div class='nested'>Central Vacuum Solution #2B - 6 Inlets, kit with Power Head and Hose</div>
+                <div class='nested'>Camera For CCTV Zuum Media System (Including Wiring, PoE Injector, No NVR)</div>
+                <div class='nested'>CCTV Zuum Media System - (NVR with 4 TB HDD + 4 CCTV 4.0 MP IP Cameras)</div>
+                <div class='nested'>Clare Camera 2MP For CCTV System (Including Wiring, PoE Injector, 16GB Storage, No NVR)</div>
+                <div class='nested'>Clare Camera 4MP For CCTV System (Including Wiring PoE Injector, 32GB Storage, No NVR)</div>
+                <div class='nested'>Clare CCTV System - (NVR 4-ch with 1TB HDD + 4CCTV 2.0 MP IP Cameras)</div>
+                <div class='nested'>Clare CCTV System - (NVR 8-ch with 2TB HDD + 4CCTV 4.0 MP IP Cameras)</div>
+                <div class='nested'>Up To 65" TV Wallmount Installation</div>
+                <div class='nested'>Soundbar Wallmount Installation</div>
 
             </div>
             <div class='nested'>

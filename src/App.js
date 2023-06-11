2829bd550b43261e8c45a3086c740576df593a5f
aarminoe <aarminoe@gmail.com>
2023-06-11 10:19:54 -0500

    api gateway and lambda funciton set up in aws, route tested on frontend to put and get

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import History from './History';
 import NavBar from './NavBar';
@@ -11,8 +12,31 @@ import { Account } from './Account';
 import SignUp from './SignUp';
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://0dgaw8bfm0.execute-api.us-east-2.amazonaws.com/dev/jobs')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+
+  function handleTest() {
+    fetch('https://0dgaw8bfm0.execute-api.us-east-2.amazonaws.com/jobs', {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        tech: 'anoe',
+        date: 'One more'
+      })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   return (
     <div className="App">
+      <button onClick={handleTest}>put</button>
       <h1><NavBar /></h1>
       <Account>
         <Login />

42dd93f9c015aceb51cf1b7361143e83dc0d9ca0
aarminoe <aarminoe@gmail.com>
2023-09-11 22:04:50 -0500

    setting up history, show more button will allow you to see how many pieces were inputted. stage is specific

--- src/HistoryJob.js
+++ src/HistoryJob.js
@@ -7,21 +7,148 @@ function HistoryJob({job}) {
     console.log(job)
 
     return(
+        <div>
         <div className="grid-container">
             <div className="nested">Date: {job.date}</div>
             <div className="nested">Technician: {job.aaTech}</div>
             <div className="nested">Stage: {job.aaStage}</div>
             <div className="nested">Job: {job.job}</div>
             <div className="nested">To Pay: ${job.aaToPay}</div>
-            <div className="nested">
+            <div>
                 <button onClick={() => setShowMore(!showMore)}>Show More</button>
             </div>
+        </div>
             {showMore ?
             <div>
-                
+                {job.aaStage == 'rough' ?
+                <div className="grid-container">
+                    <div>
+                    <div class='nested'>
+                        STRUCTURED WIRING - ROUGH-IN DESCRIPTION
+                    </div>
+                    <div class='nested'>
+                        Cat5/6, RG6 Wire Drop
+                    </div>
+                    <div class='nested'>
+                        Structural Panel Installation
+                    </div>
+                    <div class='nested'>
+                        Distribution Prewire Option 1
+                    </div>
+                    <div class='nested'>
+                        Distribution Prewire Option 2
+                    </div>
+                    <div class='nested'>
+                        Distribution Prewire Option 3
+                    </div>
+                    <div class='nested'>
+                        Alarm Wire Prewire
+                    </div>
+                    <div class='nested'>
+                        Alarm Door Contact Prewire
+                    </div>
+                    <div class='nested'>
+                        Audio Distribution Per Zone (Pair) Prewire
+                    </div>
+                    <div class='nested'>
+                        Local/Global Audio Source
+                    </div>
+                    <div class='nested'>
+                        5.1-2 Surround Prewire
+                    </div>
+                    <div class='nested'>
+                        7.1-2 Surround Prewire
+                    </div>
+                    <div class='nested'>
+                        CCTV Prewire (Cat5/6) Prewire
+                    </div>
+                    <div class='nested'>
+                        FPPW - Same Stud Bay (Up To 10') - HDMI, (1) RG6, (2) CAT5e
+                    </div>
+                    <div class='nested'>
+                        FPPW - Same Wall (up to 35') - HDMI, (1) RG6, (2) CAT5e
+                    </div>
+                    <div class='nested'>
+                        FPPW - Same Room (Up To 50') - HDMI, (1) RG6, (2) CAT5e
+                    </div>
+                    <div class='nested'>
+                        FPPW - Remote (Over 50') - (1) HD Balun Set, (1) RG6, (3) CAT5e
+                    </div>
+                    <div class='nested'>
+                        Additional HDMI (Up To 50")
+                    </div>
+                    <div class='nested'>
+                        Vacuum Rough (Per Outlet) - Rough
+                    </div>
+                    <div class='nested'>
+                        Chase Pipe - Attic
+                    </div>
+                    <div class='nested'>
+                        Chase Pipe - Local
+                    </div>
+                    </div>
+                    <div class='nested'>
+                        COST
+                        <div class='nested'>$15.00</div>
+                        <div class='nested'>$25.00</div>
+                        <div class='nested'>$30.00</div>
+                        <div class='nested'>$45.00</div>
+                        <div class='nested'>$60.00</div>
+                        <div class='nested'>$15.00</div>
+                        <div class='nested'>$20.00</div>
+                        <div class='nested'>$35.00</div>
+                        <div class='nested'>$25.00</div>
+                        <div class='nested'>$75.00</div>
+                        <div class='nested'>$100.00</div>
+                        <div class='nested'>$20.00</div>
+                        <div class='nested'>$15.00</div>
+                        <div class='nested'>$25.00</div>
+                        <div class='nested'>$50.00</div>
+                        <div class='nested'>$60.00</div>
+                        <div class='nested'>$10.00</div>
+                        <div class='nested'>$55.00</div>
+                        <div class='nested'>$50.00</div>
+                        <div class='nested'>$30.00</div>
+                    </div>
+                    <div class='nested'>
+                        TOTAL
+                        <div class='nested'>1</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                        <div class='nested'>2</div>
+                    </div>
+                </div>
+                :null}
+                {job.aaStage == 'trim' ?
+                <div>
+                    I am a trim
+                </div>
+                :null}
+                {job.aaStage == 'activation' ?
+                <div>
+                    I am an activation
+                </div>
+                :null}
             </div>
             :null}
         </div>
+
     )
 }
 
